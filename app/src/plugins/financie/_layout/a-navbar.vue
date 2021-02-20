ae9c218336e5d5660e2f4047c5a8ef606e72648a
bugalamatej <mbugala123@gmail.com>
2021-02-20 23:47:52 +0100

    a-navbar.vue creating

--- app/src/plugins/financie/_layout/a-navbar.vue
+++ app/src/plugins/financie/_layout/a-navbar.vue
@@ -1,42 +1,57 @@
 <template>
 	<div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar toggleable="lg" type="light" variant="light">
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      <b-navbar-nav class="row">
+        <button type="button" class="btn btn-light" disabled>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+          <b-navbar  type="light">
+            <b-navbar-brand>
+            <img src="https://www.freeiconspng.com/thumbs/wallet-icon/wallet-icon-7.png">
+            </b-navbar-brand>
+          </b-navbar>
+        
+          <b-navbar-brand>
+                financie.sk
+          </b-navbar-brand>
+      
+        
+        
       </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
+      
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
+          
 
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+       
+      
+    
+      
+        
+        <b-navbar-nav class="ml-auto">
+          
+          <b-nav-item-dropdown text="Osobné financie" right>
+            <b-dropdown-item href="#"></b-dropdown-item>
+            <b-dropdown-item href="#"></b-dropdown-item>
+            <b-dropdown-item href="#"></b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown text="Pre školy" right>
+            <b-dropdown-item href="#">Základné školy</b-dropdown-item>
+            <b-dropdown-item href="#">Stredné školy</b-dropdown-item>
+            <b-dropdown-item href="#">Vysoké školy</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+        </b-navbar-nav>
+
+
+        <b-icon icon="search"></b-icon>
+   
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -46,5 +61,20 @@ export default {
 </script>
 
 <style lang = "scss" scoped>
+img{
+  height: 24px;
+  width: 22px;
+}
 	
+  .logo-text{
+    display: flex;
+  }
+
+  .blue{
+    color: cadetblue;
+  }
+
+  .navbar{
+    height: 63px;
+  }
 </style>
