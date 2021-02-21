2e871082af8a4fee4ccf402ce3dad8c6b38129e1
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-02-21 22:21:39 +0100

    Update a-navbar.vue

--- app/src/plugins/financie/_layout/a-navbar.vue
+++ app/src/plugins/financie/_layout/a-navbar.vue
@@ -1,38 +1,19 @@
 <template>
 	<div>
-    <b-navbar toggleable="lg" type="light" variant="light">
+  <b-navbar toggleable="lg" type="light" variant="light">
+    
 
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-navbar-nav class="row">
-        <button type="button" class="btn btn-light" disabled>
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
-          <b-navbar  type="light">
-            <b-navbar-brand>
-              <img class="logo-financie" src="../../../assets/logo.png" alt="logo">
-            </b-navbar-brand>
-            
-          </b-navbar>
-        
-          <b-navbar-brand>
-                
-          </b-navbar-brand>
-      
-        
-        
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="mr-auto">
+
+      <img class="financie" src="../../../assets/logo.png" alt="logo">
+
       </b-navbar-nav>
 
       
-
-          
-
-       
-      
-    
-      
-        
-        <b-navbar-nav class="ml-auto">
+      <b-navbar-nav class="ml-auto">
           
           <b-nav-item-dropdown text="Osobné financie" right>
             <b-dropdown-item href="#"></b-dropdown-item>
@@ -46,13 +27,14 @@
             <b-dropdown-item href="#">Vysoké školy</b-dropdown-item>
           </b-nav-item-dropdown>
 
+          <b-nav-item>
+              <b-icon icon="search"></b-icon>
+          </b-nav-item>
+
         </b-navbar-nav>
-
-
-        <b-icon icon="search"></b-icon>
-   
-    </b-navbar>
-  </div>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
 
 <script>
@@ -62,24 +44,12 @@ export default {
 </script>
 
 <style lang = "scss" scoped>
-img{
-  height: 24px;
-  width: 22px;
-}
-	
-  .logo-text{
-    display: flex;
+  .financie{
+    width: 120px;
+    min-height: 38px;
   }
-
-  .blue{
-    color: cadetblue;
-  }
-
   .navbar{
     height: 63px;
   }
-  .logo-financie{
-    min-width: 120px;
-    min-height: 38px;
-  }
+  
 </style>
