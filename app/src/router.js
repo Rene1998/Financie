47cf70d4e2a940019b6480369ce5bc67effeba38
Rene1998 <rene1998@yandex.com>
2021-02-25 20:36:36 +0100

    router + .env vars

--- app/src/router.js
+++ app/src/router.js
@@ -7,10 +7,21 @@ const routes = [
 	{
 		path: '/',
 		name: 'Financie',
+		meta: { title: `${process.env.DOMAIN_TITLE} | home` },
+		/*component: () =>
+			import('./plugins/financie/_layout/.vue')*/
 	},
 	{
 		path: '/pre-skoly',
 		name: 'Pre Skoly',
+		meta: { title: `${process.env.DOMAIN_TITLE} | Pre skoly` },
+		component: () =>
+			import('./plugins/financie/pre-skoly/pre-skoly.vue')
+	},
+	{
+		path: '/osobne-financie',
+		name: 'Osobne financie',
+		meta: { title: `${process.env.DOMAIN_TITLE} | Osobne financie` },
 		component: () =>
 			import('./plugins/financie/pre-skoly/pre-skoly.vue')
 	}
