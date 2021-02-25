78dc1e21dbfea283d064b5082f204be33ecae2e3
KuKo-G <kubkogymersky@gmail.com>
2021-02-25 13:45:19 +0100

    test slider

--- app/src/plugins/financie/home/z-slider.vue
+++ app/src/plugins/financie/home/z-slider.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
     </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+</div>
 </template>
+<script>
+
+</script>
 <style lang = "scss" scoped>
 
 </style>
