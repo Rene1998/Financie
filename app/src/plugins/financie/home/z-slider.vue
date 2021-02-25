73a0f836709e7843e2fd459ec9a4f74f57ad2f94
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-02-25 17:25:54 +0100

    Update z-slider.vue

--- app/src/plugins/financie/home/z-slider.vue
+++ app/src/plugins/financie/home/z-slider.vue
@@ -1,26 +1,25 @@
 <template>
-<div>
-  <b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    indicators
-    img-width="1024"
-    img-height="480"
-  >
-    <b-carousel-slide
-      caption="First slide"
-      img-src="https://picsum.photos/1024/480/?image=10"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Second Slide"
-      img-src="https://picsum.photos/1024/480/?image=12"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Third Slide"
-      img-src="https://picsum.photos/1024/480/?image=22"
-    ></b-carousel-slide>
-  </b-carousel>
+  <div class="container">
+    <div id="carouselContent" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active text-center p-4">
+                 <h5>Vedeli ste, že...</h5>
+                 <h1>Vstúpiť do II. piliera je možné len do dovŕšenia 35 rokov.</h1>
+            </div>
+            <div class="carousel-item text-center p-4">
+                
+                <p>Jokadej babka</p>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselContent" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselContent" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </div>
 </template>
 <script>
@@ -29,5 +28,18 @@ export default {
 }
 </script>
 <style lang = "scss" scoped>
-  
+  .carousel-control-prev-icon {
+    background-color: #19283B;
+  }
+  .carousel-control-next-icon{
+    background-color: #19283B;
+  }
+  .carousel-item h1{
+    font-size: 30px;
+    color: #19283B;
+  }
+  .carousel-item h5{
+    font-size: 20px;
+    color: #19283B;
+  }
 </style>
