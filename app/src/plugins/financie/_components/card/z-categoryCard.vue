8bd4f549b3c38cb47c9858fdfe1cda62afef2b05
bugalamatej <mbugala123@gmail.com>
2021-03-13 19:45:32 +0100

    Update z-categoryCard.vue

--- app/src/plugins/financie/_components/card/z-categoryCard.vue
+++ app/src/plugins/financie/_components/card/z-categoryCard.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="card d-flex align-items-center flex-column text-center">
       <div class="card-body d-flex align-items-center flex-column">
-        <img class="category-card" src="/assets/rocnik1.png" alt="">
+        <img class="category-card" src="/assets/rocnik1.png" alt="" />
         <h6 class="card-title">1 - 2 Ročník</h6>
       </div>
     </div>
@@ -10,13 +10,25 @@
 </template>
 
 <style lang="scss" scoped>
-.category-card { 
-    height: 75px;
-    width: 75px;
+.category-card {
+  height: 75px;
+  width: 75px;
 }
 .card {
-    bottom: 80px;
-    border: none;
-}
+  bottom: 80px;
+  border: none;
 
+  background: #ffffff;
+  box-shadow: 0px 4px 20px rgba(177, 177, 177, 0.15);
+  border-radius: 4px;
+
+  border-bottom: 5px solid transparent;
+  &:hover {
+    border-bottom: 5px solid #1eaee1;
+    border-radius: 5px;
+  }
+  &:focus {
+    border-bottom: 5px solid #1eaee1;
+  }
+}
 </style>
