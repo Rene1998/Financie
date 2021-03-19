20033a762813c37047267958d590728cc8b97ece
Rene1998 <rene1998@yandex.com>
2021-03-19 12:45:37 +0100

    added moment and formated time in img card / textcard

--- app/src/plugins/financie/_components/card/z-textCard.vue
+++ app/src/plugins/financie/_components/card/z-textCard.vue
@@ -3,7 +3,7 @@
     <div class="card border-0 " v-for="ztcard in ztcards" :key="ztcard.id">
       <div class="card-body p-3">
         <p class="timestamp">
-          {{ztcard.created_at}}
+          {{posted_at}}
         </p>
         <h5 class="card-text">
           {{ztcard.content}}
@@ -19,6 +19,7 @@
 </template>
 <script>
 import apiService from "../../common/apiService"
+import moment from 'moment'
 
 export default {
   name: "z-textCard",
@@ -26,6 +27,11 @@ export default {
     return {
       ztcards: [],
     };
+  },
+  computed: {
+    posted_at(){
+      return moment(this.ztcards.created_at).lang("sk").format('LL')
+    }
   },
   async mounted() {
     await this._loadCards();
