a552d0b5f2b9a8e6b9d1c09caddf3f71730f66a8
KuKo-G <kubkogymersky@gmail.com>
2021-03-19 14:06:52 +0100

    no je to tam

--- app/src/plugins/financie/_components/card/z-homeCategoryCard.vue
+++ app/src/plugins/financie/_components/card/z-homeCategoryCard.vue
@@ -1,26 +1,39 @@
 <template>
-<div>
+  <div>
     <div class="card d-flex align-items-center flex-column text-center">
       <div class="card-body d-flex align-items-center flex-column mt-3">
-        <b-icon class="mt-4" icon="house-door-fill"></b-icon>
-        <h6 class="card-title m-0 mt-2 d-flex align-items-end">Domov</h6>
+        <img class="category-card " :src="cardIcon" alt="" />
+        <h6 class="card-title m-0 mt-2 d-flex align-items-end">
+          {{ card.title }}
+        </h6>
       </div>
     </div>
   </div>
-
 </template>
-
-
-
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    cardIcon() {
+      return require(`./assets/${this.card.icon}`);
+    },
+  },
+};
+</script>
 <style lang="scss" scoped>
 .category-card {
   height: 75px;
   width: 75px;
 }
 .card {
-    cursor: pointer;
-    height: 168px;
-    width: 350px;
+  cursor: pointer;
+  height: 168px;
+  width: 350px;
   border: none;
 
   background: #ffffff;
@@ -31,27 +44,31 @@
   &:hover {
     border-bottom: 6px solid #1eaee1;
     border-radius: 4px;
-    .b-icon{
-        color: #1eaee1;
+    .b-icon {
+      color: #1eaee1;
     }
   }
   &:focus {
     border-bottom: 6px solid #1eaee1;
     border-radius: 4px;
-    .b-icon{
-        color: #1eaee1;
+    .b-icon {
+      color: #1eaee1;
     }
   }
-  
 }
 
-.b-icon{
-Width:32px;
-Height:34px;
+.b-icon {
+  width: 32px;
+  height: 34px;
 }
 
-h6{
-    font-size: 18px;
-    font-weight: 400;
+h6 {
+  font-size: 18px;
+  font-weight: 400;
+}
+
+.category-card {
+  height: 75px;
+  width: 75px;
 }
 </style>
--- app/src/plugins/financie/home/home.vue
+++ app/src/plugins/financie/home/home.vue
@@ -3,7 +3,10 @@
     <div class="container-fluid preskoly-main">
       <div class="container preskoly-container">
         <div class="preskoly-welcome-text">
-          <h1>Prevedieme vás životnou <br> finančnou cestou.</h1>
+          <h1>
+            Prevedieme vás životnou <br />
+            finančnou cestou.
+          </h1>
           <p>
             Získajte odborníka na kľúčové finančné momenty vo vašom živote.
           </p>
@@ -11,37 +14,19 @@
       </div>
     </div>
 
-  <div class=" container">
-    <div class="row category-cards">
-       <div class="col-4 mb-3  ">
-         <z-homeCategoryCard></z-homeCategoryCard>
-       </div>
-       <div class="col-4  mb-3 ">
-          <z-homeCategoryCard></z-homeCategoryCard>
-       </div>
-       <div class="col-4  mb-3 ">
-          <z-homeCategoryCard></z-homeCategoryCard>
-       </div>
+    <div class=" container">
+      <div class="row category-cards">
+        <div
+          v-for="card in cards"
+          :key="card.title"
+          :title="card.title"
+          :icon="card.icon"
+          class="col-4 mb-3"
+        >
+          <z-homeCategoryCard :card="card"></z-homeCategoryCard>
+        </div>
+      </div>
     </div>
-    
-
-
-    <div class="row">
-       <div class="col-4  mt-3 ">
-           <z-homeCategoryCard></z-homeCategoryCard>
-       </div>
-       <div class="col-4 mt-3 ">
-          <z-homeCategoryCard></z-homeCategoryCard>
-       </div>
-       <div class="col-4 mt-3  ">
-          <z-homeCategoryCard></z-homeCategoryCard>
-       </div>
-    </div>
-
-  </div>
-  
-
-
 
     <z-carousel></z-carousel>
 
@@ -74,22 +59,53 @@
 <script>
 export default {
   components: {
-    "z-homeCategoryCard":() => import("../_components/card/z-homeCategoryCard"), 
+    "z-homeCategoryCard": () =>
+      import("../_components/card/z-homeCategoryCard"),
     "z-carousel": () => import("./z-carousel"),
     "z-showArticles": () => import("../_components/z-showArticles"),
     "z-imgCard": () => import("../_components/card/z-imgCard"),
     "z-textCard": () => import("../_components/card/z-textCard"),
     "z-advancedImgHomeCards": () => import("./z-advancedImgHomeCards"),
   },
+  data() {
+    return {
+      cards: [
+        {
+          title: "Bývanie",
+          icon:"np_home_dark_blue.png",
+        },
+        {
+          title: "Rodina a deti",
+          icon:"np_family_dark_blue.png",
+        },
+        {
+          title: "Auto",
+          icon:"np_car_dark_blue.png",
+        },
+        {
+          title: "Dôchodok",
+          icon:"np_old_dark_blue.png",
+        },
+        {
+          title: "Exekúcia",
+          icon:"np_pay_dark_blue.png",
+        },
+        {
+          title: "Práca a dane",
+          icon:"np_work_dark_blue.png",
+        },
+      ],
+    };
+  },
 
-    /*mounted: function(){
+  /*mounted: function(){
         axios.get('http://localhost:8081/api/zaicard').then(function(response){
             this.zaicard = response.data;
         })
     }*/
 };
 </script>
-<style lang = "scss" scoped>
+<style lang="scss" scoped>
 .preskoly-container {
   display: flex;
   align-items: center;
@@ -105,8 +121,7 @@ export default {
   background-size: cover;
 }
 
-
-.category-cards{
+.category-cards {
   margin-top: -250px;
 }
 </style>
