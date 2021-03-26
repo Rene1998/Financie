69762f6d7b2e38cd6acd40b3321dda49774d3a0a
bugalamatej <mbugala123@gmail.com>
2021-03-26 18:33:41 +0100

    z-linkCard.create-styling

---
+++ app/src/plugins/financie/_components/card/z-linkCards.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card ">
+    <div class="body">
+      <p class="mt-4">31. júl 2020 • 4 min prečítanie</p>
+      <div class="pointer">
+         <a>
+        Bankové poplatky - <br />
+        zoznam banových <br />
+        poplatkov
+      </a>  
+      </div>
+     
+    </div>
+  </div>
+</template>
+<script></script>
+
+<style lang="scss" scoped>
+.card {
+  border: none;
+  width: 307px;
+  height: 175px;
+
+  p {
+    font-size: 14px;
+
+    color: #898989;
+  }
+
+  .pointer{
+      cursor: pointer;
+     
+     &:hover{
+         color: #1eaee1;
+     }
+      a{
+          font-size: 18px;
+          font-weight: 700; 
+      }
+  }  
+
+
+  
+}
+</style>
--- app/src/plugins/financie/osobne-financie/byvanie.vue
+++ app/src/plugins/financie/osobne-financie/byvanie.vue
@@ -5,39 +5,84 @@
         <div class="byvanie-welcome-text">
           <div class="row">
             <h1>Bývanie</h1>
-            <p>Chcete návod ako sa najrýchlejšie dopracovať k vlastnému bývaniu krok po kroku... </p>
+            <p>
+              Chcete návod ako sa najrýchlejšie dopracovať k vlastnému bývaniu
+              krok po kroku...
+            </p>
           </div>
         </div>
       </div>
     </div>
- <z-carousel></z-carousel>
-    <div class="container">
+    <z-carousel></z-carousel>
+    <div class="container mt-5 mb-5">
       <div class="row">
         <div class="col-3">
-            <z-miniCards></z-miniCards>
+          <z-miniCards></z-miniCards>
         </div>
         <div class="col-3">
-           <z-miniCards></z-miniCards>
+          <z-miniCards></z-miniCards>
         </div>
         <div class="col-3">
-           <z-miniCards></z-miniCards>
+          <z-miniCards></z-miniCards>
         </div>
         <div class="col-3">
-           <z-miniCards></z-miniCards>
+          <z-miniCards></z-miniCards>
         </div>
       </div>
     </div>
 
-   
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-4 d-flex align-items-center">
+          <b-icon icon="link45deg"></b-icon>
+          <h6 class="m-0">Užitočné linky</h6>
+        </div>
+        <div class="col-4 d-flex align-items-center">
+          <b-icon icon="question-circle-fill"></b-icon>
+          <h6 class="m-0 ml-1">Mohlo by vás zaujímať</h6>
+        </div>
+        <div class="col-4 d-flex align-items-center">
+          <b-icon icon="calculator-fill"></b-icon>
+          <h6 class="m-0">Kalkulačky a návody</h6>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="line col-4">
+          <div><z-linkCards></z-linkCards></div>
+          <hr />
+          <div><z-linkCards></z-linkCards></div>
+          <hr />
+          <div><z-linkCards></z-linkCards></div>
+        </div>
+
+        <div class="line col-4">
+          <div><z-linkCards></z-linkCards></div>
+          <hr />
+          <div><z-linkCards></z-linkCards></div>
+          <hr />
+          <div><z-linkCards></z-linkCards></div>
+        </div>
+
+        <div class="line col-4">
+          <div><z-linkCards></z-linkCards></div>
+          <hr />
+          <div><z-linkCards></z-linkCards></div>
+          <hr />
+          <div><z-linkCards></z-linkCards></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   components: {
-
     "z-carousel": () => import("../home/z-carousel"),
     "z-miniCards": () => import("../_components/card/z-miniCards"),
+    "z-linkCards": () => import("../_components/card/z-linkCards"),
   },
 };
 </script>
@@ -54,6 +99,15 @@ export default {
     display: flex;
     align-items: center;
     min-height: 500px;
+  }
+}
+
+.b-icon {
+  cursor: pointer;
+  width: 28px;
+  height: 28px;
+  &:hover {
+    color: #1eaee1;
   }
 }
 </style>
