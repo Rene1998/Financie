389ebc2e7353d1238514cf3b3e51ee9869e1015f
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-04-19 16:29:49 +0200

    Update routes.php

--- ocms/plugins/financie/imgcard/routes.php
+++ ocms/plugins/financie/imgcard/routes.php
@@ -13,3 +13,10 @@ Route::get('api/imgCard/categories', function(){
     return Category::all();
 });
 
+Route::get('api/imgCard/categories/{id}', function($id){
+    return Category::findOrFail($id);
+});
+
+Route::get('api/imgCard/categories/slug/{slug}', function($slug){
+    return Category::where('slug', $slug)->firstOrFail();
+});
