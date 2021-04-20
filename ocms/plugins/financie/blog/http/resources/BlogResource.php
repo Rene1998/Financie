eb91b4173cef86917020914f0eea4fc393716667
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-04-20 14:38:35 +0200

    Update BlogResource.php

--- ocms/plugins/financie/blog/http/resources/BlogResource.php
+++ ocms/plugins/financie/blog/http/resources/BlogResource.php
@@ -16,9 +16,4 @@ class BlogResource extends Resource
             'category_id' => $this->category_id,
         ];
     }
-
-    private static function _parseUrl($html){
-        $base_url = URL::to('');
-        return preg_replace("~src=[\"]([^']+)[\"]~", 'src="' . $base_url .'$1' . '"', $html);
-    }
 }
