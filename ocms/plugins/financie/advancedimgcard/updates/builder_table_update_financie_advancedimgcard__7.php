6c9337a8041093ba9e92b73b907f87893971c9d7
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-04-29 18:04:40 +0200

    be

--- ocms/plugins/financie/advancedimgcard/updates/builder_table_update_financie_advancedimgcard__7.php
+++ ocms/plugins/financie/advancedimgcard/updates/builder_table_update_financie_advancedimgcard__7.php
@@ -3,7 +3,7 @@
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class BuilderTableUpdateFinancieAdvancedimgcard6 extends Migration
+class BuilderTableUpdateFinancieAdvancedimgcard7 extends Migration
 {
     public function up()
     {
