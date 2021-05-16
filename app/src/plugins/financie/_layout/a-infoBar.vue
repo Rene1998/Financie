3023d580e2c5bd35977703d05226654ad92a7b62
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-05-16 16:10:57 +0200

    Update a-infoBar.vue

--- app/src/plugins/financie/_layout/a-infoBar.vue
+++ app/src/plugins/financie/_layout/a-infoBar.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="container-fluid d-flex justify-content-center text-center">
-    <div class="row d-flex flex-sm-col text-center">
-      <p class="pl-1">
+    <div class="row d-flex flex-sm-col">
+      <p class=" d-flex align-items-center mt-0 mb-0">
         Informácie ktoré vám pomôžu k správnemu investovaniu.
+        <a class=" d-flex align-items-center" href="">
+          Sledujte naše video podcasty</a
+        >
       </p>
-      <a class="pl-1" href=""> Sledujte naše video podcasty</a>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .container-fluid {
+  font-family: Helvetica Neue;;
   color: white;
   min-height: 35px;
   background-color: #ffa800;
   a {
     color: #3a4757;
+  }
+
+  @media (max-width: 600px) {
+    p {
+      font-size: 12px;
+      display:block !important;
+    }
   }
 }
 </style>
