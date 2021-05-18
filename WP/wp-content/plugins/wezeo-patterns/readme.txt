15d94443b034b3f2755006791dd295810290a86a
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-05-18 18:44:40 +0200

    wp

---
+++ WP/wp-content/plugins/wezeo-patterns/readme.txt
@@ -0,0 +1,7 @@
+== Description ==
+Ako pridat dalsi pattern ?
+v register-patterns.php si treba zduplikovat uz nejaky hotovy pattern (register_block_pattern)
+zmenit prvy parameter (pattern-name) -> bpc/pattern-name (unikatny nazov)
+zmenit title, description
+categories nemenime
+content: tu vkladame html kod nasho patternu, ktory si ale najprv minifikujeme napr. tu http://minifycode.com/html-minifier/ a takto vlozime
---
+++ WP/wp-content/plugins/wezeo-patterns/wezeo-patterns.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Wezeo patterns collection
+ * Plugin URI:        https://www.wezeo.com
+ * Description:       Wezeo collection of block patterns
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Wezeo
+ * Author URI:        https://www.wezeo.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wezeo-patterns
+ * Domain Path:       /languages
+ */
+
+require_once 'patterns/register-patterns.php';
