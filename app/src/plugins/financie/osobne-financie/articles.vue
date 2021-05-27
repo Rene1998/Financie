1b8d97d9d1fa9a1db650a633430fc5211474d7b1
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2021-05-27 21:23:39 +0200

    articles.vue create

---
+++ app/src/plugins/financie/osobne-financie/articles.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="container">
+      <h1 class="mt-5">Clanok</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae ipsum
+        incidunt rem similique. Velit, nostrum maxime. Magnam quisquam ipsa unde
+        velit repellendus, earum sequi incidunt necessitatibus optio minus
+        labore! Maxime corrupti officiis labore saepe dolorem? Eius eaque a eum
+        ab autem quam totam veritatis, odit sit pariatur aperiam cum commodi.
+        Cum voluptas doloribus, veniam temporibus obcaecati amet? Nemo iste, ut
+        exercitationem perferendis est soluta iure adipisci, architecto officiis
+        debitis vero recusandae sequi sint repellat magni cum ex ea nobis quod
+        cupiditate pariatur! Veritatis libero est quasi quod accusamus quibusdam
+        quos, molestias nam laudantium quia distinctio omnis accusantium
+        aspernatur, at iure id, consequuntur magnam repellendus eveniet rerum
+        exercitationem commodi deserunt corporis. Excepturi accusantium corrupti
+        minima repellendus error reiciendis minus delectus asperiores obcaecati.
+        Quasi ullam quas placeat, reprehenderit, cum rem molestias praesentium
+        maxime ut laudantium culpa adipisci sapiente labore quos iste? Corrupti
+        quis ut deleniti dignissimos obcaecati. Soluta, veritatis animi impedit,
+        distinctio quia dicta repudiandae itaque ducimus natus, minima dolorem
+        voluptates est tempore quo illo ratione tempora quisquam voluptatem
+        nesciunt nostrum tenetur quibusdam aut! Provident officiis sunt
+        voluptate incidunt maxime eaque blanditiis, adipisci illum architecto
+        soluta aspernatur voluptas voluptatum explicabo alias, hic tempore qui
+        modi labore perspiciatis! Deleniti quidem enim iure alias.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang = "scss" scoped>
+
+h1 {
+  margin-top: 30px;
+  color: #192949;
+  max-width: 800px;
+  justify-content: center;
+  font-family: Helvetica Neue;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 50px;
+}
+
+</style>
