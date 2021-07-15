173e270f6ac84b1c4f1007e5f685dae8a54961d4
bugalamatej <mbugala123@gmail.com>
2021-07-15 15:46:15 +0200

    visiwig elements

---
+++ app/src/ve-components/cards/ve-homeCategoryCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- wp:group -->
+    <div class="wp-block-group">
+        <div class="wp-block-group__inner-container">
+            <!-- wp:columns {"backgroundColor":"white"} -->
+            <div class="wp-block-columns has-white-background-color has-background">
+                <!-- wp:column {"width":""} -->
+                <div class="wp-block-column">
+                    <!-- wp:image {"align":"center","id":30,"width":34,"height":36,"sizeSlug":"large","linkDestination":"none"} -->
+                    <div class="wp-block-image">
+                        <figure class="aligncenter size-large is-resized">
+                            <img 
+                                src="http://localhost:8080/wp-content/uploads/2021/07/home-7.png" 
+                                alt="" 
+                                class="wp-image-30" 
+                                width="34" 
+                                height="36"
+                            />
+                        </figure>
+                    </div>
+                    <!-- /wp:image -->
+
+                    <!-- wp:paragraph {"align":"center"} -->
+                    <p class="has-text-align-center">
+                        Byvanie
+                    </p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+        </div>
+    </div>
+    <!-- /wp:group -->
+</template>
+
+
+<style  scoped>
+
+</style>
