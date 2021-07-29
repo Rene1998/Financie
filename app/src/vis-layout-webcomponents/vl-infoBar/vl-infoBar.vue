c9b38188d1b48589ca95682258cfd5b117a4daa7
Marko Hrivnák <markohrivnak80@gmail.com>
2021-07-29 13:02:52 +0200

    fake class

--- app/src/vis-layout-webcomponents/vl-infoBar/vl-infoBar.vue
+++ app/src/vis-layout-webcomponents/vl-infoBar/vl-infoBar.vue
@@ -5,7 +5,7 @@
 			<div class="row d-flex flex-sm-col">
 				<p class="d-flex align-items-center justify-content-center mt-0 mb-0">
 					Informácie ktoré vám pomôžu k správnemu investovaniu.
-					<a class="d-flex justify-content-center" target="blank" href="">
+					<a class="d-flex justify-content-center fake" target="blank" href="">
 						Sledujte naše video podcasty
 					</a>
 				</p>
