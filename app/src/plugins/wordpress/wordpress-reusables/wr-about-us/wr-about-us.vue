2f9a1d078a987d395de3ad5743746fff3843a71d
Marko Hrivnák <markohrivnak80@gmail.com>
2021-10-14 19:19:03 +0200

    adding reusables from wordpress

---
+++ app/src/plugins/wordpress/wordpress-reusables/wr-about-us/wr-about-us.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="container-fluid about-us-welcome d-flex align-items-center justify-content-center">
+            <h2 class="text-center mb-3">
+                Sme Vašou cestou <br>
+                financiami
+            </h2>
+            <p>
+                Zistite o nás viac
+            </p>
+            <p>
+                <i class="bi bi-arrow-down"></i>
+            </p>
+        </div>
+        <div class="container about-us mt-5">
+            <section class="title">
+                <h3>O nás</h3>
+                <p class="mb-5">
+                    Kto sme a čo je naše poslanie?
+                </p>
+            </section>
+            <section class="content">
+                <p class="mb-3">
+                    Sme nezisková organizácia Financie Slovensko. Našim cieľom a poslaním je prispievať k zlepšovaniu finančnej gramotnosti na školách a medzi obyvateľmi.
+                </p>
+                <p class="mb-3">
+                    Financie sú pre nás vášňou a napĺňa nás o nich vzdelávať a pomáhať ľuďom, aby peniaze neboli jadrom ich starostí.
+                </p>
+                <p class="mb-1">
+                    Vidíme ako kľúčové o financiách vzdelávať a rozprávať sa už na základnej škole. Chceme, <br> 
+                    preto učiteľom čo najviac zjednodušiť ich prácu, aby vedeli študentom poskytnúť to najlepšie finančné vzdelanie.
+                </p>
+            </section>
+            <section>
+                <p class="mt-5 mb-3">
+                    <b>Názov organizácie:</b> Financie Slovensko n. o. 
+                </p>
+                <p class="mb-3">
+                    <b>Právna forma:</b> Nezisková organizácia poskytujúca všeobecne prospešné služby
+                </p>
+                <p class="mb-3">
+                    <b>IČO:</b> 53870425
+                </p>
+                <p class="mb-5">
+                    <b>Registrový úrad:</b> Okresný úrad Bratislava
+                </p>
+                <p class="mb-3">
+                    V prípade že by ste nás chceli finančne podporiť a podporiť tak finančnú gramotnosť na Slovensku, môžete tak učiniť na tento účet:
+                </p>
+                <p class="mb-3">
+                    IBAN Financie Slovensko n. o. : <b>SK82 0900 0000 0051 8147 5269</b>
+                </p> 
+            </section>
+        </div>
+    </div>
+</template>
