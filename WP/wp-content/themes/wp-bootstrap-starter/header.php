75d0270ca386e8136d79e8378d06428693164080
Marko Hrivnák <60384671+MarkoHrivnak@users.noreply.github.com>
2022-02-09 16:10:08 +0100

    .

--- WP/wp-content/themes/wp-bootstrap-starter/header.php
+++ WP/wp-content/themes/wp-bootstrap-starter/header.php
@@ -17,7 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <script src="https://unpkg.com/vue@latest/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script>window.ocmsHost='https://financie.hybridlab.dev/ocms/api/'</script>
